9c965ea310c30dfacfd86e9c5ebc3f4f68033d2a
imranhussbd <imranhussbd@gmail.com>
2022-08-05 17:21:59 -0400

    changes

--- src/main/java/com/bestbuy/qa/objects/HomePage.java
+++ src/main/java/com/bestbuy/qa/objects/HomePage.java
@@ -7,42 +7,37 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.bestbuy.qa.common.Commons;
 
-
-
 public class HomePage {
 
-	WebDriver driver;
 	Commons commons;
-	
+
 	public HomePage(WebDriver driver, Commons commons) {
 		PageFactory.initElements(driver, this);
-		this.driver = driver;
 		this.commons = commons;
-		
+
 	}
-	
+
 	@FindBy(id = "gh-search-input")
 	WebElement searchElement;
-	
+
 	@FindBy(className = "header-search-button")
 	WebElement searchButtonElement;
-	
+
 	@FindBy(xpath = "//a[text() = 'Shop Featured PS5']")
 	WebElement shopFtP5ButtonElement;
-	
+
 	private void inputSearch(String value) {
 		commons.inputValues(searchElement, value);
 	}
-	
+
 	private void clickSearch() {
 		commons.click(searchButtonElement);
 	}
-	
+
 	private void clickShopFtPs5() {
 		commons.click(shopFtP5ButtonElement);
 	}
-	
-	
+
 	public void homepageSteps(String value) {
 		inputSearch(value);
 		clickSearch();
