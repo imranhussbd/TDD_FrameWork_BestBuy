17aaa63e3d2f8f5d4a712f4f82920a9a4a27f5e1
imranhussbd <imranhussbd@gmail.com>
2022-08-11 20:40:01 -0400

    updates

--- src/test/java/com/bestbuy/qa/home/test/HomeParameterTestClass.java
+++ src/test/java/com/bestbuy/qa/home/test/HomeParameterTestClass.java
@@ -1,18 +1,16 @@
 package com.bestbuy.qa.home.test;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.bestbuy.qa.base.BaseClass;
 
-public class HomePs5TestClass extends BaseClass {
+public class HomeParameterTestClass extends BaseClass {
 	
+	@Parameters("searchValue")
 	@Test
-	public void getResults() {
-		homePage.homepageSteps("Best Buy | Official Online Store | Shop Now & Save", "PS5");
+	public void inputSearch(String searchValue) {
+		homePage.homepageSteps(searchValue);
 		ps5PackagePage.ps5PgSteps("PlayStation 5 Consoles and Accessories", "https://www.bestbuy.com/site/playstation-5/playstation-5-packages/pcmcat1588107358954.c?id=pcmcat1588107358954");
-
 	}
 }
--- src/test/java/com/bestbuy/qa/home/test/HomeTestClass.java
+++ src/test/java/com/bestbuy/qa/home/test/HomeTestClass.java
@@ -1,16 +1,16 @@
-package com.bestbuy.qa.parameter.test;
+package com.bestbuy.qa.home.test;
 
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 
 import com.bestbuy.qa.base.BaseClass;
 
-public class SearchInputTestClass extends BaseClass {
+public class HomeTestClass extends BaseClass {
 	
-	@Parameters("searchValue")
 	@Test
-	public void inputSearch(String searchValue) {
-		homePage.homepageSteps(searchValue);
+	public void getResults() {
+		homePage.homepageSteps("Best Buy | Official Online Store | Shop Now & Save", "PS5");
 		ps5PackagePage.ps5PgSteps("PlayStation 5 Consoles and Accessories", "https://www.bestbuy.com/site/playstation-5/playstation-5-packages/pcmcat1588107358954.c?id=pcmcat1588107358954");
+
 	}
 }
