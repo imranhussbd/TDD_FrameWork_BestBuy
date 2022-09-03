1d68132e12a41f988015d3df1622e1fd1f344090
imranhussbd <imranhussbd@gmail.com>
2022-09-03 19:19:29 -0400

    update groups

---
+++ src/test/java/com/bestbuy/qa/home/all/test/AllTestSteps.java
@@ -0,0 +1,22 @@
+package com.bestbuy.qa.home.all.test;
+
+import org.testng.annotations.Test;
+
+import com.bestbuy.qa.base.BaseClass;
+
+public class AllTestSteps  extends BaseClass{
+
+	@Test(groups = "ps5")
+	public void ps5PackageVerify() {
+		homePage.homepageSteps("Best Buy | Official Online Store | Shop Now & Save", "PS5");
+		ps5PackagePage.ps5PgSteps("PlayStation 5 Consoles and Accessories", "https://www.bestbuy.com/site/playstation-5/playstation-5-packages/pcmcat1588107358954.c?id=pcmcat1588107358954");
+
+	}
+	
+	@Test(groups = "account")
+	public void createAccVerfied() {
+		homePage.homepageAccountSteps();
+		createAccount.createAccountSteps("Best Buy: Create an Account", "Create an Account", "Aniki",
+				"Saske", "test@testng.com", "classname123", "classname123", "Your passwords match!", "2125558989");
+	}
+}
--- src/test/java/com/bestbuy/qa/home/test/HomeTestClass.java
+++ src/test/java/com/bestbuy/qa/home/test/HomeTestClass.java
@@ -14,4 +14,6 @@ public class HomeTestClass extends BaseClass {
 		ps5PackagePage.ps5PgSteps("PlayStation 5 Consoles and Accessories", "https://www.bestbuy.com/site/playstation-5/playstation-5-packages/pcmcat1588107358954.c?id=pcmcat1588107358954");
 
 	}
+	
+	
 }
